683b4bd2587611f9c07535ec79f2455bf4df94bd
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-16 17:30:36 +0300

    Add movie id column to db

--- Movies/Migrations/20230216122237_AddMovieIdColumn.Designer.cs
+++ Movies/Migrations/20230216122237_AddMovieIdColumn.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Movies.Data;
 
@@ -11,9 +12,11 @@ using Movies.Data;
 namespace Movies.Migrations
 {
     [DbContext(typeof(MovieDbContext))]
-    partial class MovieDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230216122237_AddMovieIdColumn")]
+    partial class AddMovieIdColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

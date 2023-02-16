245f6e99613feaa91701a896ce508d6fc72309a5
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-17 00:39:05 +0300

    Clean up Program.cs

--- Movies/Program.cs
+++ Movies/Program.cs
@@ -5,7 +5,6 @@ using Movies.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<MovieDbContext>(options =>
 {
@@ -18,11 +17,9 @@ builder.Services.AddAutoMapper(typeof(Program));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 

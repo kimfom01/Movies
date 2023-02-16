7b90fa2125e0fc94390a22387361142f2e77c874
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-16 18:21:41 +0300

    Update model properties

--- Movies/Models/Movie.cs
+++ Movies/Models/Movie.cs
@@ -3,9 +3,10 @@
 public class Movie
 {
     public int Id { get; set; }
+    public int MovieId { get; set; }
     public required string Title { get; set; }
     public required string ImageUrl { get; set; }
-    public required string Rating { get; set; }
+    public double Rating { get; set; }
     public required string Genre { get; set; }
-    public DateTime ReleaseDate { get; set; }
+    public int Year { get; set; }
 }

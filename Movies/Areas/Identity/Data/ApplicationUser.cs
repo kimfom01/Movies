7c19fa1a407f5a651866288634b7bc71542d679e
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-05-10 13:17:26 +0300

    Add navigation to movies view and home page

--- Movies/Areas/Identity/Data/ApplicationUser.cs
+++ Movies/Areas/Identity/Data/ApplicationUser.cs
@@ -2,7 +2,6 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Movies.Areas.Identity.Data;
 
-// Add profile data for application users by adding properties to the MoviesUser class
 public class ApplicationUser : IdentityUser
 {
     public required string FirstName { get; set; }

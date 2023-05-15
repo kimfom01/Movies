793d3f6a2351e317ea8ff1e9712770ed42ab0616
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-05-15 09:49:40 +0300

    Add details page for liked movies

--- Movies/Controllers/MyMoviesController.cs
+++ Movies/Controllers/MyMoviesController.cs
@@ -1,15 +1,21 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Movies.Models;
 using Movies.Repositories;
 
 namespace Movies.Controllers;
 
+[Authorize]
 public class MyMoviesController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public MyMoviesController(IUnitOfWork unitOfWork)
+    public MyMoviesController(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
     
     public IActionResult Index()
@@ -17,5 +23,24 @@ public class MyMoviesController : Controller
         var likedMovies = _unitOfWork.LikedMovies.GetEntities();
         
         return View(likedMovies);
+    }
+
+    public async Task<IActionResult> Details(int? movieId)
+    {
+        if (movieId is null)
+        {
+            return NotFound();
+        }
+
+        var movie = await _unitOfWork.Movies.GetOneEntity(mov => mov.MovieId == movieId);
+
+        if (movie is null)
+        {
+            return NotFound();
+        }
+
+        var details = _mapper.Map<Details>(movie);
+
+        return View(details);
     }
 }
